828f103e058cc5c083a5a66088b58f70f13dd1c1
thisismatthew <thisismatt@me.com>
2020-02-01 23:33:16 +1100

    ChonksWhole win condishhhh written up

---
+++ PlanetsAndCrafts/Assets/Scripts/ChonksWhole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChonksWhole : MonoBehaviour
+{
+
+    public DragGameSprite[] chonks;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool chonksComplete = true;
+        foreach (DragGameSprite chonk in chonks)
+        {
+            if (chonk.ReturnTopParent().tag != "core")
+            {
+                chonksComplete = false;
+            }  
+            if (chonksComplete)
+            {
+                Debug.Log("CHONKS COMPLETE!");
+            }
+        }
+    }
+}
